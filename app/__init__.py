4621fca2bf648cb40cbc782a66dfdcb94c038d8d
dudufaccin <duduff1508@gmail.com>
2024-11-26 16:59:56 -0300

    criando as telas do projeto, e adicionando as rotas

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
- 
+
+
 def create_app():
     app = Flask(__name__)
 
+    app.config["SECRET_KEY"] = "sua_chave_secreta"
+
     from .routes import main
+
     app.register_blueprint(main)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 main = Blueprint("main", __name__)
 
+registros = {"ganhos": [], "gastos": []}
 
-@main.route("/")
+
+@main.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+
+        descricao = request.form["descricao"]
+        valor = float(request.form["valor"])
+        tipo = request.form["tipo"]
+
+        if tipo == "ganho":
+            registros["ganhos"].append({"descricao": descricao, "valor": valor})
+        elif tipo == "gasto":
+            registros["gastos"].append({"descricao": descricao, "valor": valor})
+
+    return render_template("index.html", registros=registros)
