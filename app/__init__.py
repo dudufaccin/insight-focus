919c1e62a678113eb05bb405e64c97d0153fa884
dudufaccin <duduff1508@gmail.com>
2024-11-26 17:02:07 -0300

    finalizando o projeto

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = "sua_chave_secreta"
-
     from .routes import main
 
     app.register_blueprint(main)
